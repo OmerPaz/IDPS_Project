741bca6401e48e2ceb730088bd7e0a73630934af
Omer Paz <omerpaz28@gmail.com>
2017-01-31 15:21:40 +0200

    Alert עדכון ל
    Alert עדכון ל

--- classes/Alert.py
+++ classes/Alert.py
@@ -5,21 +5,22 @@ from email.MIMEText import MIMEText
 class Alert(object):
 	
 	def __init__(self):
-		this._srcAddr = "cyberproject11@gmail.com"
-		this._dstAddr = "cyberproject11@gmail.com"
-		this._password = "!CyberProject11"
+		self._srcAddr = 'cyberproject11@gmail.com' # src address
+		self._dstAddr = 'cyberproject11@gmail.com' # dst address
+		self._password = '!CyberProject11' # password for the src address
 
 	def send(self, subject, message):
-		this._msg = MIMEMultipart()
-		this._msg['From'] = this._srcAddr
-		this._msg['To'] = this._dstAddr
-		this._msg['Subject'] = subject
+		self._msg = MIMEMultipart()
+		self._msg['From'] = self._srcAddr
+		self._msg['To'] = self._dstAddr
+		self._msg['Subject'] = subject
 		 
-		msg.attach(MIMEText(message, 'plain'))
+		self._msg.attach(MIMEText(message, 'plain'))
 		 
 		server = smtplib.SMTP('smtp.gmail.com', 587)
 		server.starttls()
-		server.login(fromaddr, this._password)
-		text = msg.as_string()
-		server.sendmail(this._srcAddr, this._dstAddr, text)
+		server.login(self._srcAddr, self._password)
+		text = self._msg.as_string()
+		server.sendmail(self._srcAddr, self._dstAddr, text)
 		server.quit()
+		
